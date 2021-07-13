3a962b1b00c81fec7696b422707ba38c75d90c18
Gurshan Kaile <gurshankaile206@gmail.com>
2021-07-13 14:39:07 -0700

    add good fail test for 'equilateral' triangle
    Co-authored-by: Jenn B <jennifer.bordon@gmail.com>

--- Triangle.Tests/ModelTests/TriangleTrackerTests.cs
+++ Triangle.Tests/ModelTests/TriangleTrackerTests.cs
@@ -8,18 +8,24 @@ namespace Triangle.Tests
   public class TriangleTrackerTests
   {
     [TestMethod]
-  public void IsTriangleTracker_CreateTriangleWithThreeLengths_Triangle()
-{
-  TriangleTracker testTriangle = new TriangleTracker(2,4,5); 
-  // any necessary logic to prep for test; instantiating new classes, etc.
-  Assert.AreEqual("Triangle", testTriangle.IsTriangleTracker());
-}
-  [TestMethod]
-  public void IsTriangleTracker_CheckForTypeOfTriangle_Isosceles()
-  {
-    TriangleTracker testTriangle = new TriangleTracker(4,4,5);
-    Assert.AreEqual("Isosceles", testTriangle.IsTriangleTracker());
-  }
+    public void IsTriangleTracker_CreateTriangleWithThreeLengths_Triangle()
+    {
+      TriangleTracker testTriangle = new TriangleTracker(2,4,5); 
+      // any necessary logic to prep for test; instantiating new classes, etc.
+      Assert.AreEqual("Triangle", testTriangle.IsTriangleTracker());
+    }
+    [TestMethod]
+    public void IsTriangleTracker_CheckForTypeOfTriangle_Isosceles()
+    {
+      TriangleTracker testTriangle = new TriangleTracker(4,4,5);
+      Assert.AreEqual("Isosceles", testTriangle.IsTriangleTracker());
+    }
+    [TestMethod]
+    public void IsTriangleTracker_CheckForTypeOfTriangle_Equilateral()
+    {
+      TriangleTracker testTriangle = new TriangleTracker(4,4,4);
+      Assert.AreEqual("Equilateral", testTriangle.IsTriangleTracker());
+    }
 
   }
 }
